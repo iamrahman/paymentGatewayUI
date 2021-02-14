addd43a317b67768e3f3bbb8906c209d81676ce9
Inamur Rahman <inamurr@geekyants.com>
2021-02-15 02:03:24 +0530

    code structure setup

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+const App = () => {
+    return(
+        <div className="bg-orange p-8">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      'transparent': 'transparent',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'orange': '#F2994A',
+      'orange-dark': '#FF932F',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
